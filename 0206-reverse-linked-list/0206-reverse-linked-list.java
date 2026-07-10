430bb47b8cedf98cf309454249509e3b4544167f
Priya-droid-hub <priyajagtap0101@gmail.com>
2026-07-10 12:09:29 +0530

    Time: 0 ms (100%), Space: 44.2 MB (49.9%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,13 +10,15 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode front = null;
+        while(temp != null){
+            front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
         }
-        ListNode newHead = reverseList(head.next);
-        ListNode front = head.next;
-        front.next = head;
-        head.next = null;
-        return  newHead;
+        return prev;
     }
 }
