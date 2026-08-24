85436b7b13008a24d831495512fa3e3aa6f10e6f
Priya-droid-hub <priyajagtap0101@gmail.com>
2026-08-24 23:34:42 +0530

    Time: 7 ms (12.61%), Space: 45.4 MB (9.62%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> arr = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }else{
+                arr.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] anss = new int[arr.size()];
+        int m = 0;
+
+        for (int num : arr) {
+            anss[m++] = num;
+        }
+
+        return anss;
+    }
+}
